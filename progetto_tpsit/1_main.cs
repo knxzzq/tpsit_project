353dba201e467af61db46ff435bd096edf838ff1
knxzzq <simonelazzaro44@gmail.com>
2026-05-13 21:07:15 +0200

    last bugs fixed

---
+++ progetto_tpsit/1_main.cs
@@ -0,0 +1,36 @@
+﻿partial class Magazzino
+{
+    //MONITOR
+    static object monitor = new object();
+
+    //VARIABILI PACCHI PICCOLI
+    static int prodottiPiccoli = 0;
+    static int capacitaPiccoli = 15;
+
+    //VARIABILI PACCHI MEDI
+    static int prodottiMedi = 0;
+    static int capacitaMedi = 10;
+
+    //VARIABILI PACCHI GRANDI
+    static int prodottiGrandi = 0;
+    static int capacitaGrandi = 5;
+
+    static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("AVVIO MAGAZZINO");
+        Console.ResetColor();
+
+        //robot per pacchi piccoli
+        new Thread(DepositoPiccoli).Start();
+        new Thread(PrelievoPiccoli).Start();
+
+        //robot per pacchi medi
+        new Thread(DepositoMedi).Start();
+        new Thread(PrelievoMedi).Start();
+
+        //robot per pacchi grandi
+        new Thread(DepositoGrandi).Start();
+        new Thread(PrelievoGrandi).Start();
+    }
+}
