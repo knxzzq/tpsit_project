d799339c021b3424384b685ee3997fe31ba209a4
DomeMa <147493653+DomenicoMarescaa@users.noreply.github.com>
2026-05-13 16:32:15 +0200

    final version added

---
+++ 2_piccoli.cs
@@ -0,0 +1,52 @@
+partial class Magazzino
+{
+    //REPARTO PACCHI PICCOLI
+    static void DepositoPiccoli()
+    {
+        while (true)
+        {
+            lock (monitor)
+            {
+                //se e' gia' pieno aspetta
+                if (prodottiPiccoli == capacitaPiccoli)
+                {
+                    Monitor.Wait(monitor); //sospende e aspetta
+                }
+
+                prodottiPiccoli++;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Deposito PACCHI PICCOLI: ho depositato un pacco. Totale: " + prodottiPiccoli);
+                Console.ResetColor();
+
+                //avvisa gli altri che c'e' qualcosa
+                Monitor.PulseAll(monitor);
+            }
+            Thread.Sleep(800);
+        }
+    }
+
+    static void PrelievoPiccoli()
+    {
+        while (true)
+        {
+            lock (monitor)
+            {
+                //se non c'e' nulla si ferma
+                if (prodottiPiccoli == 0)
+                {
+                    Monitor.Wait(monitor); //sospende e aspetta
+                }
+                
+                prodottiPiccoli--;     
+                Console.ForegroundColor = ConsoleColor.Red;        
+                Console.WriteLine("Prelievo PACCHI PICCOLI: ho prelevato un pacco. Totale: " + prodottiPiccoli);
+                Console.ResetColor();
+                
+                
+                //avvisa gli altri che c'e' di nuovo spazio
+                Monitor.PulseAll(monitor);
+            }
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ 4_grandi.cs
@@ -0,0 +1,55 @@
+using System.Drawing;
+
+partial class Magazzino
+{
+    // REPARTO PACCHI GRANDI
+    static void DepositoGrandi()
+    {
+        while (true)
+        {
+            lock (monitor)
+            {
+                //se e' gia' pieno aspetta
+                if (prodottiGrandi == capacitaGrandi)
+                {
+                    Monitor.Wait(monitor); //sospende e aspetta
+                }
+
+                prodottiGrandi++;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Deposito PACCHI GRANDI: ho depositato un pacco. Totale: " + prodottiGrandi);
+                Console.ResetColor();
+
+                //avvisa gli altri che c'e' qualcosa
+                Monitor.PulseAll(monitor);
+            }
+            Thread.Sleep(1800);
+        }
+    }
+
+    static void PrelievoGrandi()
+    {
+        while (true)
+        {
+            lock (monitor)
+            {
+                //se non c'e' nulla si ferma
+                if (prodottiGrandi == 0)
+                {
+                    Monitor.Wait(monitor); //sospende e aspetta
+                }
+
+                prodottiGrandi--;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Prelievo PACCHI GRANDI: ho prelevato un pacco. Totale: " + prodottiGrandi);
+                Console.ResetColor();
+
+
+
+                //avvisa gli altri che c'e' di nuovo spazio
+                Monitor.PulseAll(monitor);
+            }
+            Thread.Sleep(2000);
+        }
+    }
+}
